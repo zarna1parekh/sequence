574c98ea841dacd8af1d8c8a110b0179dd9b015b
Hansal Shah <hnshah@linkedin.com>
2020-07-10 15:46:34 -0700

    1. Added unwanted files to gitignore 2. Renamed the file to endpoints, so we can add all the endpoints in one place 3. Added doc strings to the member functions in util

--- sequence-be/app/app.py
+++ sequence-be/app/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from board_setup import board_setup_blueprint
+from endpoints import endpoints_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(board_setup_blueprint)
+app.register_blueprint(endpoints_blueprint)
 
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to teh Sequence Game!'
+    return 'Welcome to the Sequence Game!'
 
 
 if __name__ == '__main__':
--- sequence-be/app/utils.py
+++ sequence-be/app/utils.py
@@ -10,10 +10,19 @@ class Deck:
         self.num_cards = 2
 
     def get_cards_stats(self):
+        """
+        Gets the statistics of the cards left in the deck and in discarded pile
+        :return: Returns nothing, instead prints to stdout
+        """
         print("Total number of cards in the deck: {0}".format(len(self.game_deck)))
         print("Total number of cards in discarded pile: {0}".format(len(self.discarded_pile)))
 
     def setup_board(self, total_players=2):
+        """
+        Set up the board game for the required number of players
+        :param total_players: An integer representing total number of players
+        :return: Returns the map of player_id and the card for each player as dict
+        """
         self.game_deck = pydealer.Deck()
         self.discarded_pile = pydealer.Stack()
         self.game_deck.shuffle()
@@ -32,6 +41,12 @@ class Deck:
         return self.cards_dealt_map
 
     def deal_cards(self, player_id, num=1):
+        """
+        Function to deal required cards to a particular player
+        :param player_id: An integer represeting the player_id
+        :param num: An integer representing total number of cards to be deal
+        :return: Returns the map of player_id and the card for each player as dict
+        """
         player_name = "player_{0}".format(player_id)
         if player_name in self.cards_dealt_map:
             cards_dealt = self.game_deck.deal(num)
@@ -45,4 +60,8 @@ class Deck:
         return self.cards_dealt_map
 
     def get_player_cards(self):
+        """
+        Gets all the cards for each player in dict
+        :return: Returns a dict where player_id is the key and card list is the value
+        """
         return self.cards_dealt_map
