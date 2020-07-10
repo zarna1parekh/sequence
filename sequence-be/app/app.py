14b527d9e5468cea920d0f84285549f188e503bd
Hansal Shah <hnshah@linkedin.com>
2020-07-10 12:04:36 -0700

    1. Added couple of endpoints to setup the board and deal cards. 2. created util file to add the deck manipulation functions in it 3. Added blueprints to the file

--- sequence-be/app/app.py
+++ sequence-be/app/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from board_setup import board_setup_blueprint
 
 app = Flask(__name__)
+app.register_blueprint(board_setup_blueprint)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Welcome to teh Sequence Game!'
 
 
 if __name__ == '__main__':
